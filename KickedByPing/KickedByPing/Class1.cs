25bfdca412f9290fd468792cc8abfbba286aa4a3
Kacper Marcinkiewicz <marcinkiewicz.kacper@gmail.com>
2019-03-14 09:36:12 +0100

    Add KBP

---
+++ KickedByPing/KickedByPing/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Chat;
+using System.Collections.Generic;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+
+namespace KickedByPing
+{
+    
+        public class Test : IRocketCommand
+        {
+            
+
+            public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+            public string Name => "hi";
+
+            public string Help => "10s dealy hello";
+
+            public string Syntax => "<player>";
+
+            public List<string> Aliases => new List<string>();
+
+            public List<string> Permissions => new List<string>() { "myplugin.permission" };
+
+            public void Execute(IRocketPlayer caller, string[] command)
+            {
+                //float x = 4.613555F;
+                //int y = (int)x;
+                //Rocket.Core.Logging.Logger.Log("Ping: " + y.ToString() + "ms");
+
+
+                MainClass.isWaiting = true;
+                //MainClass.lastCalled = DateTime.Now;
+            }
+        }
+    
+}
---
+++ KickedByPing/KickedByPing/wait.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Chat;
+using System.Collections.Generic;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Plugins;
+using Rocket.Core.Logging;
+using System.Linq;
+using System.Text;
+
+
+using SDG.Unturned;
+
+namespace KickedByPing
+{
+    public class MainClass : RocketPlugin
+    {
+        public static DateTime lastCalled;
+        public static bool isWaiting = false;
+        public static UnturnedPlayer Player;
+
+        List<SteamPlayer> pList;
+
+        void checkPing()
+        {
+            //if ((DateTime.Now - lastCalled).TotalSeconds >= 10)
+            //{
+                float ping;
+                int intPing;
+
+                SteamPlayer sPlayer = pList[0];
+                ping = sPlayer.ping;
+                intPing = (int)ping;
+
+                UnturnedChat.Say(intPing.ToString());
+                isWaiting = false;
+            //}
+        }
+
+        public void FixedUpdate()
+        {
+            pList = Provider.clients;
+
+            if (isWaiting == true)
+            {
+                checkPing();
+            }
+        }
+    }
+}
